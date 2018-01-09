a5059c140fe9256949c8562f21b7e7a8f3d44df9
Matthias <matthias@visionspol.eu>
2018-01-09 12:38:13 +0000

    working test

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 var bodyParser = require('body-parser');
 var expressValidator = require('express-validator');
 var mongoose = require('mongoose');
