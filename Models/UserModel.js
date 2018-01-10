8d6ecbdb2d106dd06406e40b7f2dd205d57e0e03
Matthias <matthias@visionspol.eu>
2018-01-10 10:01:40 +0000

    changed mail"

--- Models/UserModel.js
+++ Models/UserModel.js
@@ -1,5 +1,4 @@
 var mongoose = require('mongoose');
-require('mongoose-type-email');
 var Data = require("../Models/DataModel");
 var Purpose = require("../Models/PurposeModel");
 var Service = require('../Models/ServiceModel');
