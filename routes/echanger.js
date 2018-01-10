e9c0c1b3d162ae04ec08039d9d808535c7328735
Matthias <matthias@visionspol.eu>
2018-01-10 10:49:46 +0000

    data condition

--- routes/echanger.js
+++ routes/echanger.js
@@ -5,8 +5,12 @@ var userModel = require("../Models/UserModel")
 router.get('/', function(req,res){
 userModel.findOne({name : req.cookies.userID}).exec(function(err, user){
         if(err) console.log(err)
+let noData = false 
+if(user.Data.FacebookWork.length==0 && user.Data.FacebookEducation.length==0 && user.Data.FacebookPosts.length==0 && user.Data.TwiiterPosts.length==0 && user.Data.GoogleDriveData.length==0) {noData = true}
 
-     res.render('echanger', {wallet : user.Wallet, payed1 : user.Payed.service1, payed2 : user.Payed.service2, payed3 : user.Payed.service3,})
+
+     res.render('echanger', {wallet : user.Wallet, payed1 : user.Payed.service1, payed2 : user.Payed.service2, payed3 : user.Payed.service3, noData : noData})
+       
         
     })
 })
